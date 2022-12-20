3dad1050918f189fbdfbbcdb95fbbdd9ddaf5c9b
clement <clement.saunier@epitech.eu>
2022-12-20 18:33:13 +0100

    [Add] Backpack page

--- src/components/elements/profile/Calendar.jsx
+++ src/components/elements/profile/Calendar.jsx
@@ -6,11 +6,23 @@ import Title from "../layout/Title";
 export default function Calendar() {
   return (
     <Card shadow={true} style={styles.container}>
-      <Title style={{backgroundColor: theme.colors.accent.green}} text="December"><Text>{"< >  "}</Text></Title>
-      <Title style={{paddingHorizontal: theme.spacing.m, backgroundColor: theme.colors.accent.yellow}}>{["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((day, index) => <Text key={index}>{day}</Text>)}</Title>
-      <Card>
-        
-      </Card>
+      <Title
+        style={{ backgroundColor: theme.colors.accent.green }}
+        text="December"
+      >
+        <Text>{"< >  "}</Text>
+      </Title>
+      <Title
+        style={{
+          paddingHorizontal: theme.spacing.m,
+          backgroundColor: theme.colors.accent.yellow
+        }}
+      >
+        {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].map((day, index) => (
+          <Text key={index}>{day}</Text>
+        ))}
+      </Title>
+      <Card></Card>
     </Card>
   );
 }
--- src/components/pages/account/Backpack.jsx
+++ src/components/pages/account/Backpack.jsx
@@ -1,10 +1,38 @@
 import { Text, View } from "react-native";
+import { theme } from "../../../style/theme.style";
+import Card from "../../elements/layout/Card";
+import GridList from "../../elements/layout/GridList";
+import Title from "../../elements/layout/Title";
 import Page from "../Page";
 
 export default function Backpack() {
   return (
     <Page>
-      <Text>Backpack</Text>
+      <Card shadow={true}>
+        <Title
+          text="Goal"
+          style={{ backgroundColor: theme.colors.accent.yellow }}
+        ></Title>
+        {[
+          { goal: "Goal 1", description: "Something about goal" },
+          { goal: "Goal 2", description: "Something about goal" },
+          { goal: "Goal 3", description: "Something about goal" }
+        ].map(({ goal, description }) => (
+          <View>
+            <Text>{goal}</Text>
+            <Text>{description}</Text>
+          </View>
+        ))}
+      </Card>
+      <Card shadow={true}>
+        <Title
+          text="Equipment"
+          style={{ backgroundColor: theme.colors.accent.green }}
+        />
+        <GridList title="Skin">{[...Array(10)].map((_, i) => i)}</GridList>
+        <GridList title="Clothes">{[...Array(15)].map((_, i) => i)}</GridList>
+        <GridList title="Head">{[...Array(5)].map((_, i) => i)}</GridList>
+      </Card>
     </Page>
   );
 }
