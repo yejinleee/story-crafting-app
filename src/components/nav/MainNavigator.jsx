6ba8b9c0475a7f4d899606a045806abc27d12e41
clement <clement.saunier@epitech.eu>
2022-12-20 18:10:58 +0100

    [Fix] Labels on tabBar

--- src/components/nav/MainNavigator.jsx
+++ src/components/nav/MainNavigator.jsx
@@ -1,6 +1,6 @@
 import { SafeAreaView, View, Text, Image } from "react-native";
 import Account from "../pages/Account";
-import Home from '../pages/Home'
+import Home from "../pages/Home";
 import Workshops from "../pages/Workshops";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
@@ -8,78 +8,106 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import Posting from "../pages/Posting";
 
 const MainNavigator = () => {
-    return(
-        <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={Posting}
-          options={{
-            tabBarIcon: ({focused}) =>{
-              if (focused) {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_home_selected.png')} style={{width: 26, height: 26}} />
-                );
-              } else {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_home.png')} style={{width: 26, height: 26}} />
-                );
-              }
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Workshop"
-          component={Workshops}
-          options={{
-            tabBarIcon: ({focused}) =>{
-              if (focused) {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_workshops_selected.png')} style={{width: 26, height: 26}} />
-                );
-              } else {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_workshops.png')} style={{width: 26, height: 26}} />
-                );
-              }
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Chat"
-          component={Account}
-          options={{
-            tabBarIcon: ({focused}) =>{
-              if (focused) {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_chat_selected.png')} style={{width: 26, height: 26}} />
-                );
-              } else {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_chat.png')} style={{width: 26, height: 26}} />
-                );
-              }
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Account}
-          options={{
-            tabBarIcon: ({focused}) =>{
-              if (focused) {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_profile_selected.png')} style={{width: 26, height: 26}} />
-                );
-              } else {
-                return (
-                  <Image source={require('../../assets/tab_bar_icons/icon_profile.png')} style={{width: 26, height: 26}} />
-                );
-              }
-            },
-          }}
-        />
-      </Tab.Navigator>
-    )
-}
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Posting}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ focused }) => {
+            if (focused) {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_home_selected.png")}
+                  style={{ width: 26, height: 26 }}
+                />
+              );
+            } else {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_home.png")}
+                  style={{ width: 26, height: 26 }}
+                />
+              );
+            }
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Workshop"
+        component={Workshops}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ focused }) => {
+            if (focused) {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_workshops_selected.png")}
+                  style={{ width: 26, height: 26 }}
+                />
+              );
+            } else {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_workshops.png")}
+                  style={{ width: 26, height: 26 }}
+                />
+              );
+            }
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={Account}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ focused }) => {
+            if (focused) {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_chat_selected.png")}
+                  style={{ width: 26, height: 26 }}
+                />
+              );
+            } else {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_chat.png")}
+                  style={{ width: 26, height: 26}}
+                />
+              );
+            }
+          }
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Account}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ focused }) => {
+            if (focused) {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_profile_selected.png")}
+                  style={{ width: 26, height: 26 }}
+                />
+              );
+            } else {
+              return (
+                <Image
+                  source={require("../../assets/tab_bar_icons/icon_profile.png")}
+                  style={{ width: 26, height: 26 }}
+                />
+              );
+            }
+          }
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default MainNavigator;
